e13c25b2e574b0b4650477d997f59ed00f9760f4
xale76 <xale76@gmail.com>
2018-06-06 11:10:38 +0200

    Update Poynt.java

--- src/android/Poynt.java
+++ src/android/Poynt.java
@@ -30,8 +30,8 @@ import co.poynt.os.model.Payment;
 
 /* ale */
 import android.os.Binder;
-import co.poynt.os.services.v1.IPoyntSecondScreenService.Stub;
-import co.poynt.os.services.v1.IPoyntSecondScreenService;
+import co.poynt.os.services.v2.IPoyntSecondScreenService.Stub;
+import co.poynt.os.services.v2.IPoyntSecondScreenService;
  
 /* ale */
 
