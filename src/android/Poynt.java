14683f44a51ecbe0d53fa5746b7257ecc5b706bb
xale76 <xale76@gmail.com>
2018-06-06 11:57:57 +0200

    Update Poynt.java

--- src/android/Poynt.java
+++ src/android/Poynt.java
@@ -188,7 +188,7 @@ public class Poynt extends CordovaPlugin{
     }
  
  
-    public void showCollectAgreement(CallbackContext callbackContext) {
+    public void showCollectAgreement(final CallbackContext callbackContext) {
         try {
             Bundle options = new Bundle();
             options.putString(Intents.EXTRA_LEFT_BUTTON_TITLE, "Nope");
